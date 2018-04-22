9b2267e0274f8d27e8552a60dc25ee331a335209
damnee562 <damnee562@gmail.com>
2018-04-22 16:43:09 +0800

    Update linkedin url and add ig.

--- homepage/src/components/SocialMediaBar.js
+++ homepage/src/components/SocialMediaBar.js
@@ -14,7 +14,11 @@ const SocialMediaBar = () => {
                 <Button circular color='twitter' icon='twitter' />
             </a>
 
-            <a href='https://www.linkedin.com/in/%E8%B2%B4%E5%BB%B7-%E6%9B%BE-56920a145/' target='_blank' rel='noopener noreferrer'>
+            <a href='https://www.instagram.com/damienzeng6134/' target='_blank' rel='noopener noreferrer'>
+                <Button circular color='instagram' icon='instagram' />
+            </a>
+
+            <a href='https://www.linkedin.com/in/damien-zeng-56920a145/' target='_blank' rel='noopener noreferrer'>
                 <Button circular color='linkedin' icon='linkedin' />
             </a>
 
